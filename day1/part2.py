1264899802561e79a2fe7a3c1f1476bb65046a64
Lifip27 <115870794+Lifip27@users.noreply.github.com>
2024-09-17 19:57:57 +0300

    Add files via upload

---
+++ day1/part2.py
@@ -0,0 +1,26 @@
+def split_list(input_list):
+    result = []
+    sublist = []
+    
+    for item in input_list:
+        if item == "":  # If an empty string is encountered, start a new sublist
+            if sublist:  # Append the current sublist to result if it's not empty
+                result.append(list(map(int, sublist)))
+            sublist = []  # Reset sublist
+        else:
+            sublist.append(item)
+    
+    if sublist:  # Add the last sublist if it's not empty
+        result.append(list(map(int, sublist)))
+    
+    return result
+
+with open("input.txt", "r") as f:
+    data = f.read().strip().split("\n")
+    top = []
+    elfs = split_list(data)
+    print(elfs)
+    for i in elfs:
+        top.append(sum(i))
+    top.sort()
+    print(sum(top[-3:]))
